370d1f37884616381422133a11073dd35525d0dd
Lluis Sanchez <llsan@microsoft.com>
2017-07-14 11:22:31 +0200

    Build fixes.

--- ShowInGithub/Properties/AddinInfo.cs
+++ ShowInGithub/Properties/AddinInfo.cs
@@ -5,7 +5,7 @@ using Mono.Addins.Description;
 [assembly:Addin (
 	"ShowInGithub", 
 	Namespace = "ShowInGithub",
-	Version = "1.0.3"
+	Version = "1.0.4"
 )]
 
 [assembly:AddinName ("Show in Github")]
